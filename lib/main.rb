49fe776f50ab60da47bff06c86a917860d7afdc9
TheNullByte <enzo2813@yahoo.com>
2012-09-06 18:19:17 -0400

    changing to class

--- lib/main.rb
+++ lib/main.rb
@@ -17,11 +17,11 @@ LOG_LOCATION = 'W3SVC1/u_ex120827.log'
 @myFile = File.open(LOG_LOCATION)
 @exploits = Regexp.new(@exploit.join("|"))
 @myFile.each {|line|
-@a400.push(line) if line =~ exploits && line =~ /\s40\d\s/
-@a200.push(line) if line =~ exploits && line =~ /\s200\s/
-@a500.push(line) if line =~ exploits && line =~ /\s50\d\s/
-@other.push(line) if line =~ exploits && line !=~ /\s404\s/
-@badIP.push(line.split(/\s/)[8]) if line =~ exploits && haxIP.include?(haxIP.push(line.split(/\s/)[8]))
+@a400.push(line) if line =~ @exploits && line =~ /\s40\d\s/
+@a200.push(line) if line =~ @exploits && line =~ /\s200\s/
+@a500.push(line) if line =~ @exploits && line =~ /\s50\d\s/
+@other.push(line) if line =~ @exploits && line !=~ /\s404\s/
+@badIP.push(line.split(/\s/)[8]) if line =~ @exploits && @badIP.include?(@badIP.push(line.split(/\s/)[8]))
 }
 
 puts "<----------             LW4W 0.0.1 (09/01/2012)             ---------->
@@ -33,9 +33,9 @@ puts "<----------             LW4W 0.0.1 (09/01/2012)             ---------->
 
 The following IP's used known hacks against the system:
 "
-haxIP.each{|a| puts "--> " + a}
+@badIP.each{|a| puts "--> " + a}
 
 puts "HTTP Response code 40x"
 puts "----------------------"
-a400.each{|a| a = a.split(/\s/)[8]; puts "    " + a}
+@a400.each{|a| a = a.split(/\s/); puts "    " + a[1] + " || " + a[3] + " || " + a[4] + " || " + a[8] + " || " + a[9]}
 end
