cb56773368fea230e94b183a2d1056826a31d31a
TheNullByte <enzo2813@yahoo.com>
2012-09-06 15:33:56 -0400

    Re-structure

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,6 @@
-# This script is to be wrapped in ruby2exe and used to analyze and mail logs for IIS servers
+class LW
 
-# Author::  TheNullByte
-
-exploits = [
+@exploits = [
    '^null$',
    '/\.\./\.\./\.\./',
    '\.\./\.\./config\.sys',
@@ -48,22 +46,26 @@ exploits = [
    '=file:\/\/',
    '\.ini'
 ]
-a400 = []
-a200 = []
-a500 = []
-other = []
-haxIP = []
-LOG_LOCATION = 'W3SVC1/u_ex120827.log'
+@a400 = []
+@a200 = []
+@a500 = []
+@other = []
+@badIP = []
 
 
-myFile = File.open(LOG_LOCATION)
-exploits = Regexp.new(exploits.join("|"))
-myFile.each {|line|
-a400.push(line) if line =~ exploits && line =~ /\s40\d\s/
-a200.push(line) if line =~ exploits && line =~ /\s200\s/
-a500.push(line) if line =~ exploits && line =~ /\s50\d\s/
-other.push(line) if line =~ exploits && line !=~ /\s404\s/
-haxIP.push(line.split(/\s/)[8]) if line =~ exploits && haxIP.include?(haxIP.push(line.split(/\s/)[8]))
+def
+
+@LOG_LOCATION = 'W3SVC1/u_ex120827.log'
+
+
+@myFile = File.open(LOG_LOCATION)
+@exploits = Regexp.new(exploits.join("|"))
+@myFile.each {|line|
+@a400.push(line) if line =~ exploits && line =~ /\s40\d\s/
+@a200.push(line) if line =~ exploits && line =~ /\s200\s/
+@a500.push(line) if line =~ exploits && line =~ /\s50\d\s/
+@other.push(line) if line =~ exploits && line !=~ /\s404\s/
+@badIP.push(line.split(/\s/)[8]) if line =~ exploits && haxIP.include?(haxIP.push(line.split(/\s/)[8]))
 }
 
 puts "<----------             LW4W 0.0.1 (09/01/2012)             ---------->
@@ -80,3 +82,4 @@ haxIP.each{|a| puts "--> " + a}
 puts "HTTP Response code 40x"
 puts "----------------------"
 a400.each{|a| a = a.split(/\s/)[8]; puts "    " + a}
+end
