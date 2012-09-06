b4b14c75f7f51d345dd8db0311f6d369931ebba8
TheNullByte <enzo2813@yahoo.com>
2012-09-06 14:41:57 -0400

    RDOC change

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 LW4W
 ====
 
+This script is to be wrapped in ruby2exe and used to analyze and mail logs for IIS servers
+
+Author::  TheNullByte
+
 LW4W
--- main.rb
+++ main.rb
@@ -1,7 +1,3 @@
-# This script is to be wrapped in ruby2exe and used to analyze and mail logs for IIS servers
-
-# Author::  TheNullByte
-
 exploits = [
    '^null$',
    '/\.\./\.\./\.\./',
